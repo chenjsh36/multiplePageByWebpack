3895ada1c8e3621299b05236d706e531dc16ccf5
SKC-20160509YVK\Administrator <793494356@qq.com>
2016-11-04 14:19:21 +0800

    脚本实现复制dll文件到build目录下

--- npm-scripts/clean-before-build.js
+++ npm-scripts/clean-before-build.js
@@ -1,6 +1,17 @@
 var fs = require('fs');
 var rimraf = require('rimraf');
+var path = require('path');
 var dirVars = require('../webpack-config/base/dir-vars.config.js');
 rimraf(dirVars.buildDir, fs, function cb() {
     console.log('build 目录已经清空');
 })
+
+
+function copy(src, dst) {
+	fs.writeFileSync(dst, fs.readFileSync(src));
+}
+
+var src = path.resolve(dirVars.dllDir, 'dll.js');
+var dist = path.resolve(dirVars.buildDir, 'dll/dll.js');
+
+copy(src, dist);
