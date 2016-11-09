ccccddf7f52f52c6dd2aa11943428b1a8321cde6
SKC-20160509YVK\Administrator <793494356@qq.com>
2016-11-09 11:07:53 +0800

    添加element组件库

--- src/pages/testvue/main.js
+++ src/pages/testvue/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-default/index.css'
 import App from './part.vue'
+
+// Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 module.exports = new Vue({
-	el: '#app',
-	template: '<App/>',
-	components: { App }
-});
+    el: '#app',
+    template: '<App/>',
+    components: { App }
+})
 
-console.log('zxcv');
+console.log('zxcv')
