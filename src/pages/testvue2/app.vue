c91d3747c48869edd944900a58ce4bfd39035dfd
SKC-20160509YVK\Administrator <793494356@qq.com>
2016-11-10 14:59:03 +0800

    Use vuex to manage statuss

--- src/pages/testvue2/app.vue
+++ src/pages/testvue2/app.vue
@@ -11,24 +11,6 @@
     // 业务组件引入
     import ViewLogin from './ViewLogin/ViewLogin.vue'
 
-    // 自制事件总站
-    // import Vue from 'vue'
-
-    // var GLOBALDATA = {
-    //     loginState: false
-    // };
-    // var bus = new Vue({
-    //     data: GLOBALDATA 
-    // });
-    
-    // bus.$emit('loginState', true);
-
-    // bus.$on('loginState', function(state) {
-    //     console.log('set loginState:', state);
-    // })
-    // window.bus = bus;
-
-
     export default {
         data() {
             return {
